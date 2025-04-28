41c96faeea4d443a126b6f482b05640773923475
huumanh1612Í <21522331@gm.uit.edu.vn>
2025-04-28 12:25:45 +0700

    Add number item listener and fix view model

--- app/src/main/java/com/example/coffeeshop/ViewModel/MainViewModel.kt
+++ app/src/main/java/com/example/coffeeshop/ViewModel/MainViewModel.kt
@@ -1,24 +1,40 @@
-package com.example.coffeeshop.ViewModel
+package com.example.coffeeshop.Adapter
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
-import com.example.coffeeshop.Domain.BannerModel
-import com.example.coffeeshop.Domain.CategoryModel
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.coffeeshop.Activity.DetailActivity
 import com.example.coffeeshop.Domain.ItemsModel
-import com.example.coffeeshop.Repository.MainRepository
+import com.example.coffeeshop.databinding.ViewholderPopularBinding
 
-class MainViewModel:ViewModel() {
-    private  val repository=MainRepository()
+class PopularAdapter(val items:MutableList<ItemsModel>):
+    RecyclerView.Adapter<PopularAdapter.Viewholder>() {
+    lateinit var context: Context
+    class Viewholder(val binding: ViewholderPopularBinding):RecyclerView.ViewHolder(binding.root)
 
-    fun loadBanner():LiveData<MutableList<BannerModel>>{
-        return repository.loadBanner()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PopularAdapter.Viewholder {
+        context=parent.context
+        val binding=ViewholderPopularBinding.inflate(LayoutInflater.from(context),parent,false)
+        return Viewholder(binding)
     }
 
-    fun loadCategory():LiveData<MutableList<CategoryModel>>{
-        return repository.loadCategory()
+    override fun onBindViewHolder(holder: PopularAdapter.Viewholder, position: Int) {
+        holder.binding.titleTxt.text=items[position].title
+        holder.binding.priceTxt.text="$"+items[position].price.toString()
+
+        Glide.with(context)
+            .load(items[position].picUrl[0])
+            .into(holder.binding.pic)
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra("object",items[position])
+            context.startActivity(intent)
+        }
     }
 
-    fun loadPopular():LiveData<MutableList<ItemsModel>>{
-        return repository.loadPopular()
-    }
+    override fun getItemCount(): Int =items.size
 }
