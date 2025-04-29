8e7e2f787130597a4e116db6e10c85180d8208d6
ChauTai2003 <lordchautai@gmail.com>
2025-04-29 10:35:54 +0700

    Fix Detail Activity

--- app/src/main/java/com/example/coffeeshop/Activity/DetailActivity.kt
+++ app/src/main/java/com/example/coffeeshop/Activity/DetailActivity.kt
@@ -1,19 +1,85 @@
 package com.example.coffeeshop.Activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.example.coffeeshop.Domain.ItemsModel
 import com.example.coffeeshop.R
+import com.example.coffeeshop.databinding.ActivityDetailBinding
+import com.example.coffeeshop.Helper.ManagmentCart
 
 class DetailActivity : AppCompatActivity() {
+    lateinit var binding: ActivityDetailBinding
+    private lateinit var item:ItemsModel
+    private lateinit var  manageCart: ManagmentCart
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        setContentView(R.layout.activity_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding=ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        manageCart= ManagmentCart(this)
+
+        bundle()
+        initSizeList()
+    }
+
+    private fun initSizeList() {
+        binding.apply {
+            smallBtn.setOnClickListener {
+                smallBtn.setBackgroundResource(R.drawable.stroke_brown_bg)
+                mediumBtn.setBackgroundResource(0)
+                largeBtn.setBackgroundResource(0)
+            }
+            mediumBtn.setOnClickListener {
+                smallBtn.setBackgroundResource(0)
+                mediumBtn.setBackgroundResource(R.drawable.stroke_brown_bg)
+                largeBtn.setBackgroundResource(0)
+            }
+            largeBtn.setOnClickListener {
+                smallBtn.setBackgroundResource(0)
+                mediumBtn.setBackgroundResource(0)
+                largeBtn.setBackgroundResource(R.drawable.stroke_brown_bg)
+            }
+        }
+    }
+
+    private fun bundle() {
+        binding.apply {
+            item= intent.getSerializableExtra("object") as ItemsModel
+
+            Glide.with(this@DetailActivity)
+                .load(item.picUrl[0])
+                .into(binding.picMain)
+
+            titleTXT.text=item.title
+            narrativeTxT.text=item.description
+            priceTxt.text="$"+item.price
+            ratingTxt.text=item.rating.toString()
+
+            addtoCartBtn.setOnClickListener {
+                item.numberInCart=Integer.valueOf(
+                    numberItemTxt.text.toString()
+                )
+                manageCart.insertItems(item)
+            }
+            backBtn.setOnClickListener {
+                finish()
+            }
+            plusCart.setOnClickListener {
+                numberItemTxt.text=(item.numberInCart+1).toString()
+                item.numberInCart++
+            }
+            minusBtn.setOnClickListener {
+                if (item.numberInCart>0){
+                    numberItemTxt.text=(item.numberInCart-1).toString()
+                    item.numberInCart--
+                }
+            }
         }
     }
 }
